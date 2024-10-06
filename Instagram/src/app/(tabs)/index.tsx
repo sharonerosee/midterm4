839f9676f5d25b1152e9b89481887a023b42884a
sharonerosee <129519148+sharonerosee@users.noreply.github.com>
2024-10-06 14:34:12 +0700

    ft. responsive feed

--- Instagram/src/app/(tabs)/index.tsx
+++ Instagram/src/app/(tabs)/index.tsx
@@ -3,13 +3,14 @@ import posts from '~/assets/data/posts.json';
 import PostListItem from '~/src/components/PostListItem';
 
 export default function FeedScreen() {
-    return(
+    return (
         <FlatList
-        data={posts}
-        renderItem={({item}) => <PostListItem post={item}/>}
-        contentContainerStyle={{gap: 10}}
-        showsVerticalScrollIndicator={false}
+            data={posts}
+            className='alignItems-center'
+            renderItem={({ item }) => <PostListItem post={item} />}
+            contentContainerStyle={{ gap: 10, maxWidth: 512, width: '100%'}}
+            showsVerticalScrollIndicator={false}
         />
-    )
+    );
 
 }
